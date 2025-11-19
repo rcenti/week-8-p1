561d1571ec70f3c89f5d1b49a648c4b4e403e3f0
rcenti <vlt534@york.ac.uk>
2025-11-19 12:09:20 +0000

    done

--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -2,6 +2,11 @@ let x = 50;
 let y = 50;
 let speedX = 3;
 let speedY = 3;
+let boing;
+
+function preload(){
+    boing = loadSound("assets/194812__funnyman850__epic-angry-boatinception-sound-effect.mp3");
+}
 
 function setup() {
     createCanvas(600, 400);
@@ -12,9 +17,11 @@ function draw() {
     circle(x, y, 100);
     if (x < 50 || x > width - 50) {
         speedX *= -1;
+        boing.play();
     }
     if (y < 50 || y > height - 50) {
         speedY *= -1;
+        boing.play()
     }
     x += speedX;
     y += speedY;
